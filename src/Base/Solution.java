9a3a2732bf2fbe86a742a1d7b9d33ec3ed7327ff
SeanLeong <790513642@qq.com>
2020-10-19 22:46:32 +0800

    20.10.19

--- src/Base/Solution.java
+++ src/Base/Solution.java
@@ -79,4 +79,43 @@ public class Solution {
         }
         return new int[0];
     }
+
+    //31. 下一个排列
+    public void nextPermutation(int[] nums) {
+        //实话说，这个鬼题目我都没怎么看懂
+
+        //没有太多的思路，看来题解只是觉得巧妙的很
+
+        //题解答题的思路：从右边开始遍历数组，遇到第一个下降的元素（i-1），
+        // 在[i,length-1]这一段范围进行搜索，找到最小的并且大于i-1下标元素的元素(t)
+        //交换i-1和t，交换后，[i, length-1]是一段上升区间，翻转即可
+        //temp变量用于记录上一个值的大小, index用于记录第一个下降的下标
+        int i = nums.length - 2;
+        while(i > 0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+        if(i >= 0){
+            int j = nums.length - 1;
+            while(j > 0 && nums[j] <= nums[i]){
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i+1);
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start){
+        int i = start, j = nums.length;
+        while(i < j){
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
 }
