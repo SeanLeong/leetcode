8fbe3c9530ad5850e213c4f9c165aa24f2b77786
SeanLeong <790513642@qq.com>
2020-10-27 09:26:51 +0800

    20.10.27

--- src/Base/Solution.java
+++ src/Base/Solution.java
@@ -539,6 +539,30 @@ public class Solution {
         }
         return l;
     }
+
+    //36. 有效的数独
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] block = new boolean[9][9];
+
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(board[i][j] != '.'){
+                    int index = board[i][j] - '1';
+                    int blockIndex = (i / 3) * 3 + (j / 3);
+                    if(row[i][index] || col[j][index] || block[blockIndex][index]){
+                        return false;
+                    }else{
+                        row[i][index] = true;
+                        col[j][index] = true;
+                        block[blockIndex][index] = true;
+                    }
+                }
+            }
+        }
+        return true;
+    }
 }
 
 
